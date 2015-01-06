48a9c9a2aec13da637809fcd165bd5e2fbcbb683
jtm789 <superjtm@gmail.com>
2015-01-06 16:51:53 +0800

    mathTest class

---
+++ MathTest.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Wrox
+{
+	class MainEntryPoint{
+		static void Main(){
+			Console.WriteLine("PI is "+MathTest.GetPi());
+			int x=MathTest.GetSquareOf(5);
+			Console.WriteLine("Square of 5 is"+x);
+			
+			MathTest math=new MathTest();
+			math.value=30;
+			Console.WriteLine("Value field of math variable contains "+math.value);
+			Console.WriteLine("Square of 30 is "+math.GetSquare());
+		
+		}
+	
+	}
+	
+	
+	class MathTest{
+		public int value;
+		
+		public int GetSquare(){
+			return value * value;
+		}
+		
+		public static int GetSquareOf(int x){
+			return x*x;
+		}
+		
+		public static double GetPi(){
+			return 3.14159;
+		}
+	
+	}
+}
